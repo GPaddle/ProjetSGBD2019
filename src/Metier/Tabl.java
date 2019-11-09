817b92a444e584392664d1650346e02b95f2d85d
Keller Guillaume <keller.guillaume68@gmail.com>
2019-11-10 00:26:26 +0100

    Update Tabl.java
    Remplis la classe tabl

--- src/Metier/Tabl.java
+++ src/Metier/Tabl.java
@@ -1,5 +1,34 @@
 package Metier;
 
 public class Tabl {
-	private 
+
+	private int numTab;
+	private int nbPlace;
+	
+	
+	
+	public Tabl(int numTab, int nbPlace) {
+		super();
+		this.numTab = numTab;
+		this.nbPlace = nbPlace;
+	}
+	public int getNumTab() {
+		return numTab;
+	}
+	public void setNumTab(int numtab) {
+		this.numTab = numtab;
+	}
+	/**
+	 * @return the nbplace
+	 */
+	public int getNbPlace() {
+		return nbPlace;
+	}
+	/**
+	 * @param nbplace the nbplace to set
+	 */
+	public void setNbPlace(int nbplace) {
+		this.nbPlace = nbplace;
+	}
+	
 }
