9a7d2f2455d36e1acc43e16fa85ab3d1d8a2ecee
Keller Guillaume <keller.guillaume68@gmail.com>
2019-11-11 00:17:51 +0100

    Update IDBDD.java
    Changement du nom

--- src/Connexion/IDBDD.java
+++ src/Connexion/IDBDD.java
@@ -1,11 +1,21 @@
 package Connexion;
 
-public class ID_BDDLocal {
+public class IDBDD {
 	
 	private String url ="jdbc:oracle:thin:@localhost:1521:XE";
 	private String id ="GPaddle";
 	private String mdp ="GPaddle";
 	
+	public IDBDD(String url, String id, String mdp) {
+		super();
+		this.url = url;
+		this.id = id;
+		this.mdp = mdp;
+	}
+
+	public IDBDD() {
+	}
+
 	public String getId() {
 		return id;
 	}
